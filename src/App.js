6625d16b178f8a03f718fd4427bbfd9c05d61474
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-11-16 17:02:18 -0300

    fisrt commit: cra clean up, GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
+import { UserProvider } from './context/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<UserProvider>
+			<BrowserRouter>
+				<GlobalStyle />
+				<Routes>
+					{/* <Route path="/" element={<LoginPage/>} />
+        <Route path="/cadastro" element={<SignUpPage/>} />
+        <Route path="/hoje" element={<TodayPage />} />
+        <Route path="/habitos" element={<HabitsPage />} />
+        <Route path="/historico" element={<HistoryPage/>} /> */}
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
+	);
 }
-
-export default App;
