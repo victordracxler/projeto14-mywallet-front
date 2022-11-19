2a09b23f6768b50d06050dca96f8c4a48f582f94
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-11-19 02:30:41 -0300

    feat: get entries

--- src/components/TransactionsBoard.js
+++ src/components/TransactionsBoard.js
@@ -1,6 +1,30 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import UserContext from '../context/UserContext';
 
 export default function TransactionsBoard() {
+	const [entryList, setEntryList] = useState([]);
+	const { bearer } = useContext(UserContext);
+
+	useEffect(() => {
+		const headers = {
+			Authorization: bearer,
+		};
+
+		const url = 'http://localhost:5000/entries';
+
+		axios
+			.get(url, { headers })
+			.then((res) => {
+				setEntryList(res.data);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err.response);
+			});
+	}, []);
+
 	return (
 		<>
 			<Board></Board>
